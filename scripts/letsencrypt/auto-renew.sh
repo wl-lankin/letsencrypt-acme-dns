531327ad4b5a7cf70b5f045d79abc701e8f431a5
Wolfgang Linz <webmaster@wl-lankin.de>
2019-11-29 09:36:11 +0100

    Update auto-renew.sh

--- scripts/letsencrypt/auto-renew.sh
+++ scripts/letsencrypt/auto-renew.sh
@@ -3,10 +3,8 @@
 certbot certonly -n --manual-public-ip-logging-ok --server https://acme-v02.api.letsencrypt.org/directory --agree-tos --non-interactive --manual --preferred-challenges=dns --email email@email.de --manual-auth-hook /opt/letsencrypt/dns-auth.sh -d *.DOMAIN1.de -d DOMAIN1.de
 certbot certonly -n --manual-public-ip-logging-ok --server https://acme-v02.api.letsencrypt.org/directory --agree-tos --non-interactive --manual --preferred-challenges=dns --email email@email.de --manual-auth-hook /opt/letsencrypt/dns-auth.sh -d *.DOMAIN2.de -d DOMAIN2.de
 
-
 ### Delete if using Nginx
 apachectl -k graceful
-
 
 ### Delete if using Apache
 /etc/init.d/nginx reload
